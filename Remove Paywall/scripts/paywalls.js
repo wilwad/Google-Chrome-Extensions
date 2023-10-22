c480b93a8b9836a640b7b3369509128abd87708b
William Sengdara <william.sengdara@gmail.com>
2023-10-22 22:23:47 +0200

    Update paywalls.js

--- Remove Paywall/scripts/paywalls.js
+++ Remove Paywall/scripts/paywalls.js
@@ -1,4 +1,17 @@
-	let paywalls = [ 
-					{'site':'www.washingtonpost.com', 'selector': 'div[data-qa="overlay-container"]' } 
+let paywalls = [ 
+					{'site':'www.washingtonpost.com', 
+					 'elements': [
+					                {'selector':'div[data-qa="overlay-container"]', 'action':'remove'},
+					                {'selector':'body', 'action':'position:initial'}
+					            ]
+					} ,
+					
+					{'site':'www.nytimes.com', 
+					'elements': [ 
+					              {'selector':'div[data-testid="gateway-container"]', 'action':'remove'}, 
+					              {'selector':'.css-gx5sib', 'action':'remove'},
+					              {'selector':'div[data-testid="vi-gateway-container"]', 'action':'position:initial;overflow:initial' }
+					            ] 
+					} ,
+					
 				];
-				
